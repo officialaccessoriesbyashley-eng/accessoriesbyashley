1f139cd5a9c209a76368963ad7b94a6556ef5c75
Brabyns Yabwetsa <burabuyabz@gmail.com>
2026-05-27 21:54:19 +0300

    feat: update currency display from £ to KSh across product details and filters

--- components/app/ProductFilters.tsx
+++ components/app/ProductFilters.tsx
@@ -285,7 +285,7 @@ export function ProductFilters({ categories }: ProductFiltersProps) {
       {/* Price Range */}
       <div>
         <FilterLabel isActive={isPriceActive} filterKey="price">
-          Price Range: £{priceRange[0]} - £{priceRange[1]}
+          Price Range: KSh {priceRange[0]} - KSh {priceRange[1]}
         </FilterLabel>
         <Slider
           min={0}
